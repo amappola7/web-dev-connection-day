71a7532693d3e35b5e3c5b8122b53f3b1b73faf0
Amappola <anam.porrasp@gmail.com>
2023-03-22 13:42:12 -0500

    Mostrando los productos en el home

--- web-dev-challenge/src/Pages/Home/Home.jsx
+++ web-dev-challenge/src/Pages/Home/Home.jsx
@@ -1,16 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import { getProductsList } from '../../API/requests'
+import Header from '../Header/Header'
 
 function Home() {
   const [products, setProducts] = useState([])
+  const [result, setResult] = useState('Hola Mundo')
 
   useEffect (() => {
     getProductsList(setProducts)
   }, [])
 
+  useEffect (() => {
+    setResult(
+      products.map((product) => {
+        return(
+        <section
+          key={product.id}
+        >
+          <img src={product.images ? product.images[0] : ''} alt={product.title}/>
+          <p>{product.title}</p>
+          <p>{`$${product.price}`}</p>
+        </section>)
+      })
+    )
+  }, [products])
+
   console.log('LISTA DE PRODUCTOS:\n', products)
   return (
-    <div>Home</div>
+    <div>
+      <Header>
+      </Header>
+      <section>
+        {
+          result
+        }
+      </section>
+      <footer>
+        <p>Hecho por Squad 2</p>
+      </footer>
+    </div>
   )
 }
 
